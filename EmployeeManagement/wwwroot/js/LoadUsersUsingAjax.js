5dd8389662fdf060136e7bc0cce3eb1cff44d87e
NimitaJoshi <162729558+NimitaJoshi@users.noreply.github.com>
2024-03-08 19:00:09 +0000

    Add files via upload

---
+++ EmployeeManagement/wwwroot/js/LoadUsersUsingAjax.js
@@ -0,0 +1,28 @@
+﻿
+$(document).ready(function () {
+   
+    var eachUserTR="";
+
+    $.ajax({
+        type:"GET",
+        url:"/User/MyUserlistJsonResult",
+        dataType:'json',
+        success: function (data) {
+            $.each(data, function (i, user) {
+                eachUserTR += "<tr>" +
+                    "<td>" + user.name + "</td>" +
+                    "<td>" + user.age + "</td>" +
+                    "<td>" + user.email + "</td>" +
+                    "<td>" + user.password + "</td>" +
+                    "</tr>"
+            });
+            $("#AjaxUsersTable tbody").append(eachUserTR);
+        },
+
+        error: function (errorData) {
+            var errorJson = jQuery.parseJSON(errorData.responseText);
+            alert("Message:" +errorJson.Message);
+        }
+
+    });
+});
