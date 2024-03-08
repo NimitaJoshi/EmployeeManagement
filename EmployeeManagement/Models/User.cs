2ed7e0123606e67a6bab664317e27ddff8b9b2ea
NimitaJoshi <162729558+NimitaJoshi@users.noreply.github.com>
2024-03-08 18:58:13 +0000

    Add files via upload

---
+++ EmployeeManagement/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace EmployeeManagement.Models
+{
+    public class User
+    {
+      
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter name")]
+        public string Name{ get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Please enter age")]
+        [Range(16,65, ErrorMessage = "Enter age between 16 to 65")]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Please enter email")]       
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage ="It is not a valid email address")]
+        public string Email { get; set; }= string.Empty;
+
+        [Required(ErrorMessage = "Please enter password")]
+        public string Password { get; set; }=string.Empty;
+
+    }
+}
---
+++ EmployeeManagement/Models/UserRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace EmployeeManagement.Models
+{
+    public class UserRepository
+    {
+        public static List<User> users = new List<User>();
+
+        public static void create(User user)
+        {
+            users.Add(user);
+        }
+        public static void delete(int id)
+        {
+            User? selectedUser = users.Where(x => x.Id == id).FirstOrDefault();
+            if (selectedUser != null)
+            {
+                users.Remove(selectedUser);
+            }
+        }
+        public static void update(User User, int id)
+        {
+            User? selectedUser = users.Where(x => x.Id == id).FirstOrDefault();
+            if (selectedUser != null)
+            {
+                selectedUser.Name = User.Name;
+                selectedUser.Age = User.Age;
+                selectedUser.Email = User.Email;
+                selectedUser.Password = User.Password;
+            }
+        }
+    }
+}
